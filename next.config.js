855fe8c0e8d7831fc4394bcc8054db04f7646889
mocavada <cebuprint@gmail.com>
2023-11-15 04:06:00 -0500

    done

--- next.config.js
+++ next.config.js
@@ -1,13 +1,31 @@
-module.exports = {
+// module.exports = {
+//   images: {
+//     formats: ['image/avif', 'image/webp'],
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         port: '',
+//         pathname: '/my-account/**',
+//       },
+//     ],
+//   },
+// }
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  optimizeFonts: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/my-account/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
+    minimumCacheTTL: 1500000,
   },
-}
+};
+
+module.exports = nextConfig;
+
